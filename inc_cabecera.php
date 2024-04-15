baeef421953ebca6c72674d52cfaf36e3b5fa98c
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 11:08:17 +0200

    Metodo para conectar a la base de datos creado en Inc_cabecera

--- inc_cabecera.php
+++ inc_cabecera.php
@@ -1,12 +1,29 @@
 <?php
-class Inc_cabecera {
-static function insertarBotones(){
-    echo '<button id="buscar">Buscar</button><br><br>';
-    echo '<button id="listado">Listado</button><br><br>';
-    echo '<button id="nuevo">Nuevo</button><br><br>';
-}
-static function cabecera(){
-    echo '<h1>Ejemplo de Cabecera</h1>';
-}
-}
+class Inc_cabecera
+{
+    static function insertarBotones()
+    {
+        echo '<button id="buscar">Buscar</button><br><br>';
+        echo '<button id="listado">Listado</button><br><br>';
+        echo '<button id="nuevo">Nuevo</button><br><br>';
+    }
+    static function cabecera()
+    {
+        echo '<h1>Ejemplo de Cabecera</h1>';
+    }
 
+    static function conectar()
+    {
+        $servidor = 'localhost';
+        $usuario = 'root';
+        $contrasenia = '';
+        $bd = 'contab';
+        try {
+            $conexion = new PDO('mysql:host=' . $servidor . '; dbname=' . $bd, $usuario, $contrasenia);
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conexion;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
