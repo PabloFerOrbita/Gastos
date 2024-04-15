3578333967cad5813c138aae18c05ef53d76533e
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 10:37:45 +0200

    funcion para insertar botones en página añadida

---
+++ inc_cabecera.php
@@ -0,0 +1,7 @@
+<?php
+
+function insertarBotones(){
+    echo '<button id="buscar">Buscar<button><br><br>';
+    echo '<button id="listado">Listado<button><br><br>';
+    echo '<button id="nuevo">Nuevo<button><br><br>';
+}
