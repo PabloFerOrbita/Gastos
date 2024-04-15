c18dc68f93772f0bf10f154908ee342d18a9f32e
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 14:02:18 +0200

    el input editar en el formulario de modificar guarda el valor de la descripcion

--- modificar.php
+++ modificar.php
@@ -25,7 +25,7 @@
         echo '<input type="date" name="fecha" id="fecha" value="' .  $registro[0]['fecha'] . '"></input><br><br>';
         echo '<label for="categoria">Categoria del gasto</label><br>';
         echo '<input type="text" name="categoria" id="categoria" value=' . $registro[0]['categoria'] .  '></input><br><br>';
-        echo '<input type="hidden" name="editar" id="editar"/>';
+        echo '<input type="hidden" name="editar" id="editar" value="' . $_GET['descripcion'] . '"/>';
         echo '<button type="submit">Guardar</button>';
     }
     Inc_pie::pie();
