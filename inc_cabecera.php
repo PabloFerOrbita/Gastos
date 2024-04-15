8c154f53236dc09bf5dc5874e15e6afc172fd44b
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 10:49:33 +0200

    Etiquetas de button en insertarBotones cerradas

--- inc_cabecera.php
+++ inc_cabecera.php
@@ -1,9 +1,9 @@
 <?php
 class Inc_cabecera {
 static function insertarBotones(){
-    echo '<button id="buscar">Buscar<button><br><br>';
-    echo '<button id="listado">Listado<button><br><br>';
-    echo '<button id="nuevo">Nuevo<button><br><br>';
+    echo '<button id="buscar">Buscar</button><br><br>';
+    echo '<button id="listado">Listado</button><br><br>';
+    echo '<button id="nuevo">Nuevo</button><br><br>';
 }
 static function cabecera(){
     echo '<h1>Ejemplo de Cabecera</h1>';
