4485ff49d5e02bfc9416900d8626dbce7cd3d0a1
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 10:41:12 +0200

    declaracion de clase añadida en inc_cabecera.php

--- inc_cabecera.php
+++ inc_cabecera.php
@@ -1,7 +1,10 @@
 <?php
-
+class Inc_cabecera {
 function insertarBotones(){
     echo '<button id="buscar">Buscar<button><br><br>';
     echo '<button id="listado">Listado<button><br><br>';
     echo '<button id="nuevo">Nuevo<button><br><br>';
 }
+
+}
+
