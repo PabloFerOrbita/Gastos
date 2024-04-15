548c86578e509af575f9e8a87de0d63ae67f9f0a
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 10:43:40 +0200

    funciones de insertarBotones y cabecera convertidas en estáticas

--- inc_cabecera.php
+++ inc_cabecera.php
@@ -1,11 +1,11 @@
 <?php
 class Inc_cabecera {
-function insertarBotones(){
+static function insertarBotones(){
     echo '<button id="buscar">Buscar<button><br><br>';
     echo '<button id="listado">Listado<button><br><br>';
     echo '<button id="nuevo">Nuevo<button><br><br>';
 }
-function cabecera(){
+static function cabecera(){
     echo '<h1>Ejemplo de Cabecera</h1>';
 }
 }
