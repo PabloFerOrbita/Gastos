5907369ffdbd42d9abdb6eb875e6732ac5841934
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 10:09:27 +0200

    Botones buscar listado y nuevo añadidos

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button id="buscar">Buscar</button><br><br>
+    <button id="listado">Listado</button><br><br>
+    <button id="nuevo">Nuevo</button><br><br>
+</body>
+</html>
