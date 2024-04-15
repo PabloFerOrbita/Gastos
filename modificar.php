d39d0970ab62d6a93d89c3111d1c48e115e79516
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-15 16:31:04 +0200

    cambio del formulario y de los valores que recibe modificar.php para que reciba y envie valores del método POST además de GET

--- modificar.php
+++ modificar.php
@@ -14,13 +14,13 @@
     Inc_cabecera::cabecera();
     ?>
     <?php
-    if(isset($_GET['editar'])){
-        echo Inc_cabecera::actualizarRegistro('UPDATE gastos SET fecha = "' . $_GET['fecha'] . '", descripcion = "' . implode(" ", explode("+", $_GET['descripcion'])) . '", categoria = "' . $_GET['categoria'] . '", importe = ' . $_GET['importe'] . ' WHERE descripcion like "' . implode(" ", explode("+", $_GET['editar'])). '"' );
+    if(isset($_POST['editar'])){
+        echo Inc_cabecera::actualizarRegistro('UPDATE gastos SET fecha = "' . $_POST['fecha'] . '", descripcion = "' . implode(" ", explode("+", $_POST['descripcion'])) . '", categoria = "' . $_POST['categoria'] . '", importe = ' . $_POST['importe'] . ' WHERE descripcion like "' . implode(" ", explode("+", $_POST['editar'])). '"' );
     }
-    if (isset($_GET['descripcion'])) {
-        $registro = Inc_cabecera::recibirRegistro($_GET['descripcion']);
+    if (isset($_GET['descripcion']) || isset($_POST['descripcion'])) {
+        $registro = Inc_cabecera::recibirRegistro($_POST['descripcion'] ?? $_GET['descripcion']  );
         if ( count($registro) > 0){
-        echo '<form method="GET">';
+        echo '<form method="POST">';
         echo '<label for="descripcion">Descripcion del gasto</label><br>';
         echo '<textarea id="descripcion" name="descripcion">' . $registro[0]['descripcion'] . '</textarea><br><br>';
         echo '<label for="importe">Importe del gasto</label><br>';
@@ -29,7 +29,7 @@
         echo '<input type="date" name="fecha" id="fecha" value="' .  $registro[0]['fecha'] . '"></input><br><br>';
         echo '<label for="categoria">Categoria del gasto</label><br>';
         echo '<input type="text" name="categoria" id="categoria" value=' . $registro[0]['categoria'] .  '></input><br><br>';
-        echo '<input type="hidden" name="editar" id="editar" value="' . $_GET['descripcion'] . '"/>';
+        echo '<input type="hidden" name="editar" id="editar" value="' . $registro[0]['descripcion'] . '"/>';
         echo '<button type="submit">Guardar</button>';}
         else{
             echo 'El registro no existe';
