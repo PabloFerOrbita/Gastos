eb46304f0edbf519d0aa0aa869370623676831a5
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-17 12:38:58 +0200

    modificar.php cambiado para que reciba y envíe el index a la hora de acutalizar

--- modificar.php
+++ modificar.php
@@ -11,27 +11,27 @@
 <body>
     <?php
     require_once('inc_cabecera.php');
- 
+
     ?>
     <?php
-    if(isset($_POST['editar'])){
-        echo Inc_cabecera::actualizarRegistro('UPDATE gastos SET fecha = "' . $_POST['fecha'] . '", descripcion = "' . implode(" ", explode("+", $_POST['descripcion'])) . '", categoria = "' . $_POST['categoria'] . '", importe = ' . $_POST['importe'] . ' WHERE descripcion like "' . implode(" ", explode("+", $_POST['editar'])). '"' );
+    if (isset($_POST['editar'])) {
+        echo Inc_cabecera::actualizarRegistro('UPDATE gastos SET fecha = "' . $_POST['fecha'] . '", descripcion = "' . implode(" ", explode("+", $_POST['descripcion'])) . '", categoria = "' . $_POST['categoria'] . '", importe = ' . $_POST['importe'] . ' WHERE ID = ' . $_POST['editar']);
     }
     if (isset($_GET['descripcion']) || isset($_POST['descripcion'])) {
-        $registro = Inc_cabecera::recibirRegistro($_POST['descripcion'] ?? $_GET['descripcion']  );
-        if ( count($registro) > 0){
-        echo '<form method="POST">';
-        echo '<label for="descripcion">Descripcion del gasto</label><br>';
-        echo '<textarea id="descripcion" name="descripcion" required>' . $registro[0]['descripcion'] . '</textarea><br><br>';
-        echo '<label for="importe" required>Importe del gasto</label><br>';
-        echo '<input type="number" name="importe" id="importe" min="0.01" max="99999999" step="0.01"  value=' . doubleval($registro[0]['importe'])  . ' required></input><br><br>'; //TODO cambiar step any a 0.01, poner maximo de 8 cifras
-        echo '<label for="fecha">Fecha del gasto</label><br>';
-        echo '<input type="date" name="fecha" id="fecha" value="' .  $registro[0]['fecha'] . '" required></input><br><br>';
-        echo '<label for="categoria">Categoria del gasto</label><br>';
-        echo '<input type="text" name="categoria" id="categoria" value=' . $registro[0]['categoria'] .  ' required></input><br><br>';
-        echo '<input type="hidden" name="editar" id="editar" value="' . $registro[0]['descripcion'] . '"/>';
-        echo '<button type="submit">Guardar</button>';}
-        else{
+        $registro = Inc_cabecera::recibirRegistro($_POST['descripcion'] ?? $_GET['descripcion']);
+        if (count($registro) > 0) {
+            echo '<form method="POST">';
+            echo '<label for="descripcion">Descripcion del gasto</label><br>';
+            echo '<textarea id="descripcion" name="descripcion" required>' . $registro[0]['descripcion'] . '</textarea><br><br>';
+            echo '<label for="importe" required>Importe del gasto</label><br>';
+            echo '<input type="number" name="importe" id="importe" min="0.01" max="99999999" step="0.01"  value=' . doubleval($registro[0]['importe'])  . ' required></input><br><br>'; //TODO cambiar step any a 0.01, poner maximo de 8 cifras
+            echo '<label for="fecha">Fecha del gasto</label><br>';
+            echo '<input type="date" name="fecha" id="fecha" value="' .  $registro[0]['fecha'] . '" required></input><br><br>';
+            echo '<label for="categoria">Categoria del gasto</label><br>';
+            echo '<input type="text" name="categoria" id="categoria" value=' . $registro[0]['categoria'] .  ' required></input><br><br>';
+            echo '<input type="hidden" name="editar" id="editar" value="' . $registro[0]['ID'] . '"/>';
+            echo '<button type="submit">Guardar</button>';
+        } else {
             echo 'El registro no existe';
         }
     }
