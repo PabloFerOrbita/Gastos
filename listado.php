8b858498d5ddbbb1cd905407e29bdc2d72e09141
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-17 12:35:40 +0200

    Modificado listado.php para que pida el id en la query

--- listado.php
+++ listado.php
@@ -14,7 +14,7 @@
     ?>
     <h4>Has elegido la opción listado</h4>
     <?php
-    Inc_cabecera::MostrarLista('SELECT fecha, importe, descripcion FROM gastos ORDER BY fecha DESC');
+    Inc_cabecera::MostrarLista('SELECT ID, fecha, importe, descripcion FROM gastos ORDER BY fecha DESC');
     require_once('inc_pie.php');
     ?>
 </body>
