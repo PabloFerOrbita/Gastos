4d87419a94c46741e15318eff974947873f483dc
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-18 11:59:31 +0200

    css añadido a buscar.php

--- buscar.php
+++ buscar.php
@@ -12,17 +12,25 @@
     <?php
     require_once('inc_cabecera.php');
     ?>
-    <h4>Has elegido la opción Buscar</h4>
-    <?php
-    echo '<form method="GET">';
-    echo '<input type="text" placeholder="Buscar..." name="busqueda" id="busqueda"></input>';
-    echo '<button type="submit">Buscar</button>';
-    echo '</form><br>';
-    if(isset($_GET['busqueda'])){
-        Inc_cabecera::MostrarLista('SELECT * FROM gastos WHERE descripcion like "%' . $_GET['busqueda'] . '%" ORDER BY fecha DESC');
-    }
-    require_once('inc_pie.php');
-    ?>
+    <div class="m-2">
+        <h4 class="mb-3">Has elegido la opción Buscar</h4>
+        <?php
+        echo '<form method="GET">';
+        echo '<div class="row g-3 align-items-center">';
+        echo '<div class="col-auto">';
+        echo '<input type="text" placeholder="Introduce la busqueda..." name="busqueda" id="busqueda" class="form-control"></input>';
+        echo '</div>';
+        echo '<div class="col-auto">';
+        echo '<button type="submit" class="btn btn-danger">Buscar</button>';
+        echo '</div>';
+        echo '</div>';
+        echo '</form><br>';
+        if (isset($_GET['busqueda'])) {
+            Inc_cabecera::MostrarLista('SELECT * FROM gastos WHERE descripcion like "%' . $_GET['busqueda'] . '%" ORDER BY fecha DESC');
+        }
+        require_once('inc_pie.php');
+        ?>
+    </div>
 </body>
 
 </html>
