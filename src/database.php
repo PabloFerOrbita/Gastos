797f4ba1aec3cc8269e47b88d3c760f7653b186d
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-18 14:08:26 +0200

    metodo eliminar

--- src/database.php
+++ src/database.php
@@ -1,11 +1,13 @@
 <?php
-class Database {
+class Database
+{
     private $bd;
     private $servidor;
     private $usuario;
     private $contrasenia;
 
-    public function __construct($bd, $servidor, $usuario, $contrasenia) {
+    public function __construct($bd, $servidor, $usuario, $contrasenia)
+    {
         $this->usuario = $usuario;
         $this->contrasenia = $contrasenia;
         $this->bd = $bd;
@@ -15,9 +17,10 @@ class Database {
     /**
      * Devuelve la conexión de la base de datos
      * 
-     * @return mixed;
+     * @return mixed
      */
-    public function conexion(){
+    public function conexion()
+    {
         try {
             $conexion = new PDO('mysql:host=' . $this->servidor . '; dbname=' . $this->bd, $this->usuario, $this->contrasenia);
             return $conexion;
@@ -27,25 +30,38 @@ class Database {
         }
     }
     //TODO terminar obtener_datos
-    public function obtener_datos($tabla, $numero){
+    public function obtener_datos($tabla, $numero)
+    {
         $con = self::conexion();
         $query = $con->prepare('SELECT * FROM ' . $tabla);
-        try{
-        if($query->execute()){
-            $datos = $query->fetchAll();
-
-        }
-        
-
-        }catch(PDOException $e){
+        try {
+            if ($query->execute()) {
+                $datos = $query->fetchAll();
+            }
+        } catch (PDOException $e) {
             $e->getMessage();
         }
-        
     }
 
-    
+    /**
+     * Eliminar el registro correspondiente
+     * 
+     * @param int $ID
+     * @param string $tabla
+     * @return bool
+     */
 
-     
-
+    public function eliminar(int $ID, string $tabla) : bool
+    {
+        $con = self::conexion();
+        $query = $con->prepare('DELETE FROM ' . $tabla . ' WHERE ID = ' . $ID);
+        try {
+            if ($query->execute()) {
+                return true;
+            }
+            return false;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }
-?>
