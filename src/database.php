8c90dd1de937d09f20e47f66532790f39244efc0
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-19 11:01:31 +0200

    funcion de modificar implementada

--- src/database.php
+++ src/database.php
@@ -108,15 +108,60 @@ class Database
         }
     }
 
-    public function modificar(string $tabla, int $ID, array $camposAmodificar, array $valoresNuevos)
+    /**
+     * Modifica los valores deseados de la tabla indicada
+     * @return bool|NULL
+     * Devuelve true si se ha modificado algún parámetro, false si ningún registro se ha visto afectado y NULL si ha habido un error.
+     * @param string $tabla
+     * La tabla de la cual se quieren modificar los valores
+     * @param array $camposAmodificar
+     * Los campos que se quiere modificar
+     * @param array $valoresNuevos
+     * Los valores que se desea introducir en esos campos, en el mismo orden que el de los campos que se quieren cambiar; en caso de tener claves,
+     * tambíen deben ser las mismas.
+     * @param string $parametroBusqueda
+     * [opcional] el parámetro que indica que registro quieres modificar, en el caso de estar vacío, se modificarán todos los registros
+     * @param mixed $valorAbuscar
+     * [opcional] el valor que el campo del registro que se quiere modificar debe tener según el parámetro de búsqueda.
+     */
+
+    public function modificar(string $tabla, array $camposAmodificar, array $valoresNuevos, string $parametroBusqueda = '', mixed $valorAbuscar = 0): bool|NULL
     {
         $con = self::conexion();
         $sql = 'UPDATE ' . $tabla . ' SET ';
-        if (count($camposAmodificar) == count($valoresNuevos)) {
+        if ((count($camposAmodificar) == count($valoresNuevos)) && (array_keys($camposAmodificar) == array_keys($valoresNuevos))) {
             $ultimo_indice = array_key_last($camposAmodificar);
             foreach ($camposAmodificar as $indice => $campo) {
-                $sql .= $campo . ' = ' . $valoresNuevos[$indice];
+                if (is_string($valoresNuevos[$indice])) {
+                    $sql .= $campo . ' = "' . $valoresNuevos[$indice] . '"';
+                } else if (is_double($valoresNuevos[$indice]) || $valoresNuevos[$indice]) {
+                    $sql .= $campo . ' = ' . $valoresNuevos[$indice];
+                }
+                if ($indice !== $ultimo_indice) {
+                    $sql .= ', ';
+                }
+            }
+            if ($parametroBusqueda !== '') {
+                $sql .= ' WHERE ' . $parametroBusqueda;
+                if (is_string($valorAbuscar)) {
+                    $sql .= ' like "' . $valorAbuscar . '"';
+                } else if (is_int($valorAbuscar) || is_double($valorAbuscar)) {
+                    $sql .= ' = ' . $valorAbuscar;
+                }
+            }
+            $query = $con->prepare($sql);
+            try {
+                if ($query->execute()) {
+                    if ($query->rowCount() == 0) {
+                        return false;
+                    }
+                    return true;
+                }
+                return null;
+            } catch (PDOException $e) {
+                return $e->getMessage();
             }
         }
+        return null;
     }
 }
