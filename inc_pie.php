7e99d9fc70b2344605cf23c0ca06e21a2a28e8d3
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-22 18:18:08 +0200

    añadido fondo rojo al footer

--- inc_pie.php
+++ inc_pie.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
- <footer class="navbar fixed-bottom"><div class="container text-center"><p class="col-12">Mi contabilidad doméstica</p></div></footer>   
+ <footer class="navbar fixed-bottom bg-danger"><div class="container text-center text-white"><p class="col-12">Mi contabilidad doméstica</p></div></footer>   
 </body>
 </html>
 
