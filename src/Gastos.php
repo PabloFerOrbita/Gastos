2739fff6cf71101655cbea4f13364553ba7b72b7
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-22 10:51:56 +0200

    método eliminar_gasto creado

--- src/Gastos.php
+++ src/Gastos.php
@@ -3,6 +3,11 @@ require_once('Database.php');
 class Gastos
 {
     private $tabla = 'gastos';
+    private Database $db;
+
+    public function __construct(){
+        $this->db = new Database('contab', 'localhost', 'root', '');
+    }
 
     /**
      * Obtiene todos los datos de la tabla gastos
@@ -10,10 +15,21 @@ class Gastos
      * @return array
      * Devuelve un array con los datos de la tabla gastos
      */
-    public function obtener_todos()
+    public function obtener_todos() : array
     {
-        $database = new Database('contab', 'localhost', 'root', '');
-        return $database->obtener_datos($this->tabla);
+        return $this->db->obtener_datos($this->tabla);
+    }
+
+    /**
+     * Elimina un registro de la tabla datos según el ID que se introduzca
+     * @param int $ID
+     * EL ID del registro que se quiere eliminar
+     * @return null|bool
+     * Devolverá true si el registro se ha eliminado, false si no o null si ha habido algún error
+     * 
+     */
+    public function eliminar_gasto(int $ID){
+        return $this->db->eliminar($ID, $this->tabla);
     }
 
     
