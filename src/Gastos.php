43b5b802ab884d01308c83527219568f5785212c
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-22 10:56:10 +0200

    método obtener_gasto creado

--- src/Gastos.php
+++ src/Gastos.php
@@ -5,7 +5,8 @@ class Gastos
     private $tabla = 'gastos';
     private Database $db;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = new Database('contab', 'localhost', 'root', '');
     }
 
@@ -15,22 +16,34 @@ class Gastos
      * @return array
      * Devuelve un array con los datos de la tabla gastos
      */
-    public function obtener_todos() : array
+    public function obtener_todos(): array
     {
         return $this->db->obtener_datos($this->tabla);
     }
 
     /**
-     * Elimina un registro de la tabla datos según el ID que se introduzca
+     * Elimina un registro de la tabla gastos según el ID que se introduzca
      * @param int $ID
      * EL ID del registro que se quiere eliminar
      * @return null|bool
      * Devolverá true si el registro se ha eliminado, false si no o null si ha habido algún error
      * 
      */
-    public function eliminar_gasto(int $ID){
+    public function eliminar_gasto(int $ID) : ?bool
+    {
         return $this->db->eliminar($ID, $this->tabla);
     }
 
-    
+    /**
+     * Obtiene un registro de la tabla gastos según el ID que se introduzca
+     * @param int $ID 
+     * El ID del registro que se desea obtener
+     * @return array
+     * Devuelve un array con los datos del registro obtenido
+     */
+
+    public function obtener_gasto(int $ID) : array
+    {
+        return $this->db->obtener_datos($this->tabla, [], 'ID', $ID);
+    }
 }
