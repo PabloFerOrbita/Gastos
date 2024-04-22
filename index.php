86ca517328f33859793908db1bae088e2a2f47d5
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-22 18:07:57 +0200

    TODOs borrados

--- index.php
+++ index.php
@@ -10,9 +10,6 @@
 
 <body>
     <?php
-    //TODO leer documentación del require_once
-    //TODO mejorar el front
-    //TODO pruebas de todo
     require_once('inc_cabecera.php');
     
     echo '<h3 class="m-1">Bienvenido a mi contabilidad doméstica, actualmente hay ' . Inc_cabecera::contar() . ' gastos existentes</h3>';
