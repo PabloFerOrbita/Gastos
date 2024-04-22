a94d57790520ed4fa6a4bfa444789491d1cf37bf
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-22 10:42:30 +0200

    método obtener_todos creado

--- src/Gastos.php
+++ src/Gastos.php
@@ -1,1 +1,20 @@
 <?php
+require_once('Database.php');
+class Gastos
+{
+    private $tabla = 'gastos';
+
+    /**
+     * Obtiene todos los datos de la tabla gastos
+     * 
+     * @return array
+     * Devuelve un array con los datos de la tabla gastos
+     */
+    public function obtener_todos()
+    {
+        $database = new Database('contab', 'localhost', 'root', '');
+        return $database->obtener_datos($this->tabla);
+    }
+
+    
+}
