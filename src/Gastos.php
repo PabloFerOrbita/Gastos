34488c423d690c2fa47c75164f27390b577f7f6f
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-23 11:41:36 +0200

    metodo actualizar añadido a gastos.php

--- src/Gastos.php
+++ src/Gastos.php
@@ -58,5 +58,23 @@ class Gastos
        return $this->db->suma($this->tabla, 'importe');
     }
 
+    /**
+     * Modifica los campos deseados
+     * 
+     * @param array $datos
+     * Los campos que se quiere modificar junto con los valores que se les desean añadir, debe seguir la siguiente sintaxis
+     * [Nombre del campo => valor nuevo]
+     * @param string $filtro
+     * [opcional] el parámetro que indica que registro quieres modificar, en el caso de estar vacío, se modificarán todos los registros
+     * @param mixed $valor
+     * [opcional] el valor que el campo del registro que se quiere modificar debe tener según el parámetro de búsqueda.
+     * @return null|bool
+     * Devuelve true si se ha modificado algún parámetro, false si ningún registro se ha visto afectado y NULL si ha habido un error.
+     */
+    public function actualizar(array $datos, string $filtro = '', mixed $valor = ''): ?bool
+    {
+        return $this->db->modificar($this->tabla, $datos, $filtro, $valor);
+    }
+
 
 }
