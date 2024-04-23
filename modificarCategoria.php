b8ccd8e2606f495991cb005206403c27dbeb6fd6
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-23 16:25:52 +0200

    llamada Ajax para actualizar los datos y mensajes de error y de acierto

--- modificarCategoria.php
+++ modificarCategoria.php
@@ -13,25 +13,27 @@
     require_once('inc_cabecera.php');
     ?>
     <div class="vh-100">
+        <h4 class="m-2">Has elegido la opción Modificar categoría</h4>
+        <div id='mensaje'></div>
         <div class="container-fluid d-flex flex-row justify-content-center align-items-center h-50" id='cuerpo'>
-            <div class="col-3 border border-2 p-5 border-danger">
-                <form method="POST" id="formulario">
-                    <div class="mb-3 row g-2">
-                        <div class="col-12">
-                            <label for="nombre" class="form-label">Nombre de la categoría</label>
-                            <input type="text" id="nombre" name="nombre" class="form-control" pattern="^[^\s]+.*$" required></input>
+                <div class="col-3 border border-2 p-5 border-danger">
+                    <form method="POST" id="formulario">
+                        <div class="mb-3 row g-2">
+                            <div class="col-12">
+                                <label for="nombre" class="form-label">Nombre de la categoría</label>
+                                <input type="text" id="nombre" name="nombre" class="form-control" pattern="^[^\s]+.*$" required></input>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-12">
-                        <button type="submit" class="btn btn-danger w-100">Guardar</button>
-                    </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn btn-danger w-100">Guardar</button>
+                        </div>
 
-                </form>
+                    </form>
+
+                </div>
 
             </div>
-
         </div>
-    </div>
 </body>
 <script>
     var id = <?php if (isset($_GET['id'])) {
@@ -64,8 +66,52 @@
             }
         });
     } else {
+        $('#cuerpo').empty();
         $('#cuerpo').append('<div class="col-3 p-5 text-center"><h4>El registro no existe</h4></div>')
     }
+
+    $('#formulario').on('submit', (e) => {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: 'src/Categorias.php',
+            dataType: 'json',
+            data: {
+                'accion': 'actualizar',
+                'datos': {
+                    'nombre': $('#nombre').val()
+                },
+                'filtro': 'id',
+                'valor': id
+
+            },
+            success: (data) => {
+                if (data !== null) {
+                    if (data) {
+                        $('#mensaje').empty();
+                        $('#mensaje').removeClass();
+                        $('#mensaje').addClass('p-3 m-3 bg-success-subtle');
+                        $('#mensaje').append('<h3>Se han actualizado los datos</h3>');
+                        setTimeout(() => {$('#mensaje').empty();  $('#mensaje').removeClass();}, 2000)
+                       
+                    } else {
+                        $('#mensaje').empty();
+                        $('#mensaje').removeClass();
+                        $('#mensaje').addClass('p-3 m-3 bg-warning-subtle');
+                        $('#mensaje').append('<h3>No se ha actualizado ningún registro</h3>');
+                        setTimeout(() => {$('#mensaje').empty();  $('#mensaje').removeClass();}, 2000)
+                    }
+                } else {
+                    $('#mensaje').empty();
+                    $('#mensaje').removeClass();
+                    $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                    $('#mensaje').append('<h3>Ha habido un error a la hora de actualizar los datos</h3>');
+                    setTimeout(() => {$('#mensaje').empty();  $('#mensaje').removeClass();}, 2000)
+                }
+            }
+        })
+
+    })
 </script>
 
 </html>
