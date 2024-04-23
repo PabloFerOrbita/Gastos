a0f9b19e540826d877ae5aa327d1989fc803dff9
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-23 12:58:53 +0200

    llamada ajax para añadir categoria añadida a nuevaCategoria.php

--- nuevaCategoria.php
+++ nuevaCategoria.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 
@@ -12,10 +13,12 @@
     require_once('inc_cabecera.php');
     ?>
     <div class="vh-100">
+
         <h4 class="m-2">Has elegido la opción Nueva categoría</h4>
+        <div id='mensaje'></div>
         <div class="container-fluid d-flex flex-row justify-content-center align-items-center h-50">
             <div class="col-3 border border-2 p-5 border-danger">
-                <form method="POST">
+                <form method="POST" id="formulario">
                     <div class="mb-3 row g-2">
                         <div class="col-12">
                             <label for="nombre" class="form-label">Nombre de la categoría</label>
@@ -30,6 +33,34 @@
 
             </div>
         </div>
+        <script>
+            $('#formulario').on('submit', (e) => {
+                e.preventDefault();
+                var valores = [];
+                $('#formulario').serializeArray().forEach(element => {
+                    valores.push(element.value);
+                })
+                $.ajax({
+                    method: 'POST',
+                    url: 'src/Categorias.php',
+                    dataType: 'json',
+                    data: {
+                        'accion': 'aniadir',
+                        'datos': valores
+
+                    },
+                    success: (data) => {
+                        if (data) {
+                            $('#mensaje').addClass('p-3 m-3 bg-success-subtle');
+                            $('#mensaje').append('<h3>Se han añadido los datos</h3>');
+                        } else {
+                            $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                            $('#mensaje').append('<h3>Ha habido un error a la hora de añadir los datos</h3>');
+                        }
+                    }
+                })
+            })
+        </script>
         <?php
         require_once('inc_pie.php')
         ?>
