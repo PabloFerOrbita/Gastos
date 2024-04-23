87eb3b218602a933209ab57deced755e31e2b959
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-23 11:48:38 +0200

    html añadido a ListaCategorias.php

--- ListaCategorias.php
+++ ListaCategorias.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <div class="m-2">
+        <h4 class="mb-5">Has elegido la opción Categorias</h4>
+        <div class="container-fluid vh-100">
+            <div class="h-50 overflow-auto" id='lista'>
+
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
