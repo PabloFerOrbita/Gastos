74e5179d3bebbde79d655fccd37850b4f813663e
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-23 13:22:34 +0200

    llamada ajax para obtener el registro correspondiente añadida a modificarCategoria.php

--- modificarCategoria.php
+++ modificarCategoria.php
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
-    
+
 </body>
+<script>
+    var id = <?php if (isset($_GET['id'])) {
+                    echo ($_GET['id']);
+                } else {
+                    echo 'null';
+                } ?>;
+    if (id !== null) {
+        $.ajax({
+            method: 'POST',
+            url: 'src/Categorias.php',
+            dataType: 'json',
+            data: {
+                'accion': 'obtener',
+                'filtro': 'id',
+                'valor': id
+
+            },
+            success: data => {
+                console.log(data);
+            }
+        });
+    }
+</script>
+
 </html>
