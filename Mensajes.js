1dd8a3f00792de38cd21dda40544f011cadb01ef
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-24 13:53:03 +0200

    function MensajeExito añadida a Mensajes.js

--- Mensajes.js
+++ Mensajes.js
@@ -1,4 +1,13 @@
 class Mensajes{
 
-    
+    static MensajeExito(mensaje){
+        $('#mensaje').empty();
+        $('#mensaje').removeClass();
+        $('#mensaje').addClass('p-3 m-3 bg-success-subtle');
+        $('#mensaje').append(`<h3>${mensaje}</h3>`);
+        setTimeout(() => {
+            $('#mensaje').empty();
+            $('#mensaje').removeClass();
+        }, 2000)
+    }
 }
