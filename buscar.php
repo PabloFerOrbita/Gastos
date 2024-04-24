bf27e7d2fc2b759493752a4a9404ed6aea31ab45
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-24 10:40:57 +0200

    Buscar.php adaptado a las nuevas clases

--- buscar.php
+++ buscar.php
@@ -15,34 +15,126 @@
     <div class="m-2">
         <h4 class="mb-3">Has elegido la opción Buscar</h4>
 
-        <?php
-        echo '<form method="GET" id="formulario">';
-        echo '<div class="row g-3 align-items-center">';
-        echo '<div class="col-auto">';
-        echo '<input type="text" placeholder="Introduce la busqueda..." name="busqueda" id="busqueda" class="form-control"></input>';
-        echo '</div>';
-        echo '<div class="col-auto">';
-        echo '<button type="submit" class="btn btn-danger">Buscar</button>';
-        echo '</div>';
-        echo '</div>';
-        echo '</form><br>';
-        ?>
+
+        <form method="GET" id="formulario">
+            <div class="row g-3 align-items-center">
+                <div class="col-auto">
+                    <input type="text" placeholder="Introduce la busqueda..." name="busqueda" id="busqueda" class="form-control"></input>
+                </div>
+                <div class="col-auto">
+                    <button type="submit" class="btn btn-danger">Buscar</button>
+                </div>
+            </div>
+        </form><br>
+
         <div class="container-fluid vh-100">
             <div class="h-50 overflow-auto">
-                <?php
-                if (isset($_GET['busqueda'])) {
-                    Inc_cabecera::MostrarLista("SELECT g.id, g.fecha as fecha, g.importe as importe, g.descripcion as descripcion, c.nombre as categoria FROM gastos g  JOIN categorias c ON g.categoria_id = c.id and descripcion like '%" . str_replace("'", "''", trim($_GET['busqueda'])) . "%' ORDER BY fecha DESC");
-                }
-                require_once('inc_pie.php');
-                ?>
+                <div id='mensaje'></div>
+                <table class='table table-stripped table-bordered d-none' id='tabla'>
+                    <thead>
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Importe</th>
+                            <th>Descripcion</th>
+                            <th>Categoria</th>
+                            <th>Modificar</th>
+                        </tr>
+                    <tbody id='cuerpoTabla'>
+                    </tbody>
+                    </thead>
+                </table>
+
             </div>
         </div>
     </div>
-
+    <?php
+    require_once('inc_pie.php');
+    ?>
     <script>
-        $('#formulario').submit((e) => {
-            jQuery.trim($('#busqueda').val()).length == 0 && e.preventDefault();
-        })
+        var categorias = [];
+        $.ajax({
+                method: 'POST',
+                url: 'src/Categorias.php',
+                dataType: 'json',
+                data: {
+                    'accion': 'obtener',
+  
+
+                },
+                success: (data) => {
+                    categorias = data;
+                    
+                }
+            })
+        $('#formulario').on('submit', ((e) => {
+            if (jQuery.trim($('#busqueda').val()).length == 0) {
+                e.preventDefault();
+            } else {
+
+                e.preventDefault();
+                $.ajax({
+                    method: 'POST',
+                    url: 'src/Gastos.php',
+                    dataType: 'json',
+                    data: {
+                        'accion': 'obtener',
+                        'filtro': 'descripcion',
+                        'valor': $('#busqueda').val()
+
+
+                    },
+                    success: data => {
+                        $('#cuerpoTabla').empty();
+                        if (data.length > 0) {
+                           
+                            MostrarTabla(data);
+                        } else {
+                            $('#tabla').addClass('d-none');
+                            $('#mensaje').empty();
+                            $('#mensaje').removeClass();
+                            $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                            $('#mensaje').append('<h3>No se ha obtenido ningún dato</h3>');
+                            setTimeout(() => {
+                                $('#mensaje').empty();
+                                $('#mensaje').removeClass();
+                            }, 2000)
+                        }
+                    },
+                    error: () => {
+                        $('#mensaje').empty();
+                        $('#mensaje').removeClass();
+                        $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                        $('#mensaje').append('<h3>Error al conectarse al servidor</h3>');
+
+                    }
+
+                });
+            }
+            $('#busqueda').val('');
+        }));
+
+
+
+
+        async function MostrarTabla(data) {
+            data = data.sort((a, b) => {
+                return new Date(b.fecha) - new Date(a.fecha)
+            });
+            data.forEach(element => {
+                categoria = categorias.find(categoria => categoria.id == element.categoria_id)
+                let fila = $('<tr>');
+                $(fila).append(`<td>${AMDaDMA(element.fecha)}</td><td>${element.importe}</td><td>${element.descripcion.replaceAll('<', '&lt;').replaceAll('>', '&gt;')}</td><td>${categoria.nombre}</td><td><a type="button" class="btn btn-primary" href="modificar.php?ID=${element.id}">Modificar</a></td>`)
+                $('#cuerpoTabla').append(fila);
+            })
+            $('#tabla').removeClass('d-none');
+
+        }
+
+        function AMDaDMA(fecha) {
+            return fecha.split('-').reverse().join('-');
+        }
+
+         
     </script>
 </body>
 
