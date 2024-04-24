74472d4ea95a6cce9cdb8491ea75303fbf0315ea
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-24 12:27:43 +0200

    metodo RellenarGastos añadido

--- RellenarTabla.js
+++ RellenarTabla.js
@@ -1,4 +1,24 @@
-class RellenarTabla{
+class RellenarTabla {
 
-    
+    static RellenarGastos(data, categorias = []) {
+        data = data.sort((a, b) => {
+            return new Date(b.fecha) - new Date(a.fecha)
+        });
+        data.forEach(element => {
+            let fila = $('<tr>');
+            if (categorias.length > 0) {
+                $(fila).append(`<td>${AMDaDMA(element.fecha)}</td><td>${element.importe}€</td><td>${element.descripcion.replaceAll('<', '&lt;').replaceAll('>', '&gt;')}</td><td><a type="button" class="btn btn-primary" href="modificarGasto.php?id=${element.id}">Modificar</a></td>`)
+            } else {
+                let categoria = categorias.find(categoria => categoria.id == element.categoria_id);
+                $(fila).append(`<td>${AMDaDMA(element.fecha)}</td><td>${element.importe}€</td><td>${element.descripcion.replaceAll('<', '&lt;').replaceAll('>', '&gt;')}</td><td>${categoria.nombre}</td><td><a type="button" class="btn btn-primary" href="modificarGasto.php?id=${element.id}">Modificar</a></td>`)
+            }
+            $('#cuerpoTabla').append(fila);
+
+        })
+        $('#tabla').removeClass('d-none');
+    }
+
+    AMDaDMA(fecha) {
+        return fecha.split('-').reverse().join('-');
+    }
 }
