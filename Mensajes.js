7e2fa6c51d671bcfd2326fbe37e92b142110fd7e
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-24 13:54:07 +0200

    function MensajeError añadida a Mensajes.js

--- Mensajes.js
+++ Mensajes.js
@@ -1,6 +1,6 @@
-class Mensajes{
+class Mensajes {
 
-    static MensajeExito(mensaje){
+    static MensajeExito(mensaje) {
         $('#mensaje').empty();
         $('#mensaje').removeClass();
         $('#mensaje').addClass('p-3 m-3 bg-success-subtle');
@@ -10,4 +10,16 @@ class Mensajes{
             $('#mensaje').removeClass();
         }, 2000)
     }
+
+    static MensajeError(mensaje) {
+        $('#mensaje').empty();
+        $('#mensaje').removeClass();
+        $('#mensaje').addClass('p-3 m-3 bg-warning-subtle');
+        $('#mensaje').append(`<h3>${mensaje}</h3>`);
+        setTimeout(() => {
+            $('#mensaje').empty();
+            $('#mensaje').removeClass();
+        }, 2000)
+    }
+
 }
