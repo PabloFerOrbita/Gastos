e3389b33dc43cbb0332c974c9c6defd5470a0205
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-24 13:42:55 +0200

    HTML de ListaCategorias.php modificado para arreglar un problema con el scroll

--- ListaCategorias.php
+++ ListaCategorias.php
@@ -15,106 +15,109 @@
     ?>
     <div class="m-2">
         <h4 class="mb-5">Has elegido la opción Categorias</h4>
-        <div id="mensaje"></div>
         <div class="container-fluid vh-100">
             <a class="btn btn-primary mb-3" href="nuevaCategoria.php">Crear Categoria</a>
             <div class="h-50 overflow-auto">
-                <div class="container-fluid vh-100">
-                    <div class="h-50 overflow-auto">
-                        <div id='mensaje'></div>
-                        <table class='table table-stripped table-bordered d-none' id='tabla'>
-                            <thead>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Editar</th>
-                                    <th>Eliminar</th>
-                                </tr>
-                            <tbody id='cuerpoTabla'>
-                            </tbody>
-                            </thead>
-                        </table>
+                <div id="mensaje"></div>
 
-                    </div>
+                <table class='table table-stripped table-bordered d-none' id='tabla'>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    <tbody id='cuerpoTabla'>
+                    </tbody>
+                    </thead>
+                </table>
 
-                </div>
             </div>
-            <script>
-                $.ajax({
-                    method: 'POST',
-                    url: 'src/Categorias.php',
-                    dataType: 'json',
-                    data: {
-                        'accion': 'obtener'
 
-                    },
-                    success: (data) => {
-                        RellenarTabla.RellenarCategorias(data);
-                        $('.eliminar').on('click', eliminar);
-                    },
-                    error: () => {
-                        $('#tabla').addClass('d-none');
+        </div>
+    </div>
+    </div>
+    </div>
+
+    <script>
+        $.ajax({
+            method: 'POST',
+            url: 'src/Categorias.php',
+            dataType: 'json',
+            data: {
+                'accion': 'obtener'
+
+            },
+            success: (data) => {
+                RellenarTabla.RellenarCategorias(data);
+                $('.eliminar').on('click', eliminar);
+            },
+            error: () => {
+                $('#tabla').addClass('d-none');
+                $('#mensaje').empty();
+                $('#mensaje').removeClass();
+                $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                $('#mensaje').append('<h3>Error al conectarse al servidor</h3>');
+
+
+            }
+        })
+
+
+
+        function eliminar(e) {
+            $.ajax({
+                method: 'POST',
+                url: 'src/Categorias.php',
+                dataType: 'json',
+                data: {
+                    'accion': 'eliminar',
+                    'id': e.target.id
+
+                },
+                success: (data) => {
+                    if (data) {
+                        $(`#fila${e.target.id}`).remove();
+                        $('#mensaje').empty();
+                        $('#mensaje').removeClass();
+                        $('#mensaje').addClass('p-3 m-3 bg-success-subtle');
+                        $('#mensaje').append('<h3>Se ha eliminado la categoría</h3>');
+                        setTimeout(() => {
+                            $('#mensaje').empty();
+                            $('#mensaje').removeClass();
+                        }, 2000)
+                    } else {
                         $('#mensaje').empty();
                         $('#mensaje').removeClass();
                         $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
-                        $('#mensaje').append('<h3>Error al conectarse al servidor</h3>');
-
-
-                    }
-                })
-
-
-
-                function eliminar(e) {
-                    $.ajax({
-                        method: 'POST',
-                        url: 'src/Categorias.php',
-                        dataType: 'json',
-                        data: {
-                            'accion': 'eliminar',
-                            'id': e.target.id
-
-                        },
-                        success: (data) => {
-                            if (data) {
-                                $(`#fila${e.target.id}`).remove();
-                                $('#mensaje').empty();
-                                $('#mensaje').removeClass();
-                                $('#mensaje').addClass('p-3 m-3 bg-success-subtle');
-                                $('#mensaje').append('<h3>Se ha eliminado la categoría</h3>');
-                                setTimeout(() => {
-                                    $('#mensaje').empty();
-                                    $('#mensaje').removeClass();
-                                }, 2000)
-                            } else {
-                                $('#mensaje').empty();
-                                $('#mensaje').removeClass();
-                                $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
-                                $('#mensaje').append('<h3>Ha habido un error a la hora de eliminar la categoría</h3>');
-                                setTimeout(() => {
-                                    $('#mensaje').empty();
-                                    $('#mensaje').removeClass();
-                                }, 2000)
-
-                            }
-                        },
-                        error: () => {
+                        $('#mensaje').append('<h3>Ha habido un error a la hora de eliminar la categoría</h3>');
+                        setTimeout(() => {
                             $('#mensaje').empty();
                             $('#mensaje').removeClass();
-                            $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
-                            $('#mensaje').append('<h3>Error al conectarse al servidor</h3>');
-                            setTimeout(() => {
-                                $('#mensaje').empty();
-                                $('#mensaje').removeClass();
-                            }, 2000)
+                        }, 2000)
 
-                        }
-                    })
+                    }
+                },
+                error: () => {
+                    $('#mensaje').empty();
+                    $('#mensaje').removeClass();
+                    $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                    $('#mensaje').append('<h3>Error al conectarse al servidor</h3>');
+                    setTimeout(() => {
+                        $('#mensaje').empty();
+                        $('#mensaje').removeClass();
+                    }, 2000)
+
                 }
-            </script>
-            <?php
+            })
+        }
+    </script>
+    <?php
 
-            require_once('inc_pie.php');
-            ?>
+    require_once('inc_pie.php');
+    ?>
+
+
 </body>
 
 </html>
