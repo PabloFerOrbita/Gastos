47bf5401a1c80325d0b2d0dca9dec9c67f31bca2
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-24 09:38:38 +0200

    listado.php adaptado a las nuevas clases

--- listado.php
+++ listado.php
@@ -16,13 +16,72 @@
         <h4 class="mb-5">Has elegido la opción Listado</h4>
         <div class="container-fluid vh-100">
             <div class="h-50 overflow-auto">
+                <div id='mensaje'></div>
+                <table class='table table-stripped table-bordered d-none' id='tabla'>
+                    <thead>
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Importe</th>
+                            <th>Descripcion</th>
+                            <th>Modificar</th>
+                        </tr>
+                    <tbody id='cuerpoTabla'>
+                    </tbody>
+                    </thead>
+                </table>
                 <?php
-                Inc_cabecera::MostrarLista('SELECT id, fecha, importe, descripcion FROM gastos ORDER BY fecha DESC');
                 require_once('inc_pie.php');
                 ?>
             </div>
         </div>
     </div>
+    <script>
+        $.ajax({
+            method: 'POST',
+            url: 'src/Gastos.php',
+            dataType: 'json',
+            data: {
+                'accion': 'obtener_todos',
+
+            },
+            success: (data) => {
+
+                if (data.length > 0) {
+                    data = data.sort((a, b) => {
+                        return new Date(b.fecha) - new Date(a.fecha)
+                    });
+                    data.forEach(element => {
+                        let fila = $('<tr>');
+                        $(fila).append(`<td>${AMDaDMA(element.fecha)}</td><td>${element.importe}</td><td>${element.descripcion.replaceAll('<', '&lt;').replaceAll('>', '&gt;')}</td><td><a type="button" class="btn btn-primary" href="modificar.php?ID=${element.id}">Modificar</a></td>`)
+                        $('#cuerpoTabla').append(fila);
+                    })
+                    $('#tabla').removeClass('d-none');
+
+
+                } else {
+                    $('#mensaje').empty();
+                    $('#mensaje').removeClass();
+                    $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                    $('#mensaje').append('<h3>No se ha obtenido ningún dato</h3>');
+                    setTimeout(() => {
+                        $('#mensaje').empty();
+                        $('#mensaje').removeClass();
+                    }, 2000)
+                }
+            },
+            error: () => {
+                $('#mensaje').empty();
+                $('#mensaje').removeClass();
+                $('#mensaje').addClass('p-3 m-3 bg-danger-subtle');
+                $('#mensaje').append('<h3>Error al conectarse al servidor</h3>');
+
+            }
+        })
+
+        function AMDaDMA(fecha) {
+            return fecha.split('-').reverse().join('-');
+        }
+    </script>
 </body>
 
 </html>
