593e8748c957926353e8ade206e574a8d28ffd15
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-25 16:36:15 +0200

    mensaje cambiado a texto

--- Mensajes.js
+++ Mensajes.js
@@ -1,22 +1,22 @@
 class Mensajes {
 
-    static mensajeExito(mensaje) {
-        this.mensaje(mensaje, 'p-3 m-3 bg-success-subtle')
+    static mensajeExito(texto) {
+        this.mensaje(texto, 'p-3 m-3 bg-success-subtle')
     }
 
-    static mensajeAdvertencia(mensaje) {
-        this.mensaje(mensaje, 'p-3 m-3 bg-warning-subtle')
+    static mensajeAdvertencia(texto) {
+        this.mensaje(texto, 'p-3 m-3 bg-warning-subtle')
     }
 
-    static mensajeError(mensaje){
-        this.mensaje(mensaje, 'p-3 m-3 bg-danger-subtle')
+    static mensajeError(texto){
+        this.mensaje(texto, 'p-3 m-3 bg-danger-subtle')
     }
 
-    static mensaje(mensaje, clases){
+    static mensaje(texto, clases){
         $('#mensaje').empty();
         $('#mensaje').removeClass();
         $('#mensaje').addClass(clases);
-        $('#mensaje').append(`<h3>${mensaje}</h3>`);
+        $('#mensaje').append(`<h3>${texto}</h3>`);
         setTimeout(() => {
             $('#mensaje').empty();
             $('#mensaje').removeClass();
